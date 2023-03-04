aa344c51a97a10f5d10a8b3ffd518c47b5ed21d0
Azu4N0M4LY <117198296+Azu4N0M4LY@users.noreply.github.com>
2023-03-05 00:41:48 +0100

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,164 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
+local venyx = library.new("ExeHub", 5013109572)
+
+getgenv().AutoFarm = False
+getgenv().EquipBest = False
+getgenv().BestPet = False
+
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+    game:GetService("VirtualUser"):Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    wait("0."..math.random(1,9))
+    game:GetService("VirtualUser"):Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+end)
+
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+    Text = "This script have antiafk executing!";
+    Font = Enum.Font.Cartoon;
+    Color = BrickColor.new("Bright blue").Color;
+    FontSize = Enum.FontSize.Size96;    
+})
+
+local themes = {
+	Background = Color3.fromRGB(24, 24, 24),
+	Glow = Color3.fromRGB(0, 0, 0),
+	Accent = Color3.fromRGB(10, 10, 10),
+	LightContrast = Color3.fromRGB(20, 20, 20),
+	DarkContrast = Color3.fromRGB(14, 14, 14),  
+	TextColor = Color3.fromRGB(255, 255, 255)
+}
+
+
+-- local players = {
+--     "by_shadon",
+--     "lBlackD",
+--     "Naiara0615",
+--     "Seakui",
+--     "XxNotStar_x0",
+--     "Elmanuelsupremo20",
+--     "TULOVITO7W7BB",
+--     "JustAlexNoBody"
+-- }
+
+-- if players then
+--     for i = 1, #players do
+--         local player = game.Players:FindFirstChild(players[i])
+--         if player then
+--             game.StarterGui:SetCore("ChatMakeSystemMessage", {
+--                 Text = "Banned";
+--                 Font = Enum.Font.Cartoon;
+--                 Color = BrickColor.new("Bright blue").Color;
+--                 FontSize = Enum.FontSize.Size96;    
+--             })
+--             game.StarterGui:SetCore("ChatMakeSystemMessage", {
+--                 Text = "Gay";
+--                 Font = Enum.Font.Cartoon;
+--                 Color = BrickColor.new("Bright blue").Color;
+--                 FontSize = Enum.FontSize.Size96;    
+--             })
+--             game.StarterGui:SetCore("ChatMakeSystemMessage", {
+--                 Text = "Go play adopt me newbie";
+--                 Font = Enum.Font.Cartoon;
+--                 Color = BrickColor.new("Bright blue").Color;
+--                 FontSize = Enum.FontSize.Size96;    
+--             })
+--             wait(5)
+--             game:GetService("TeleportService"):Teleport(920587237)
+--         end
+--     end
+-- end
+
+
+
+
+
+
+local page = venyx:addPage("Main", 5012544693)
+local section1 = page:addSection("Credits")
+section1:addButton("Discord", function()
+    print("Discord Copied")
+    setclipboard("Invite Soon")
+end)
+
+local page = venyx:addPage("Farm")
+local section2 = page:addSection("Farm")
+section2:addToggle("AutoFarm", nil, function(bool)
+    getgenv().AutoFarm = bool
+    if bool then
+        AutoFarm()
+    end
+end)
+
+
+local page = venyx:addPage("Misc")
+local section3 = page:addSection("Misc")
+section3:addToggle("Equip Best Sword", nil, function(bool)
+    getgenv().EquipBest = bool
+    if bool then
+        EquipBest()
+    end
+end)
+
+section3:addToggle("Equip Best Pet", nil, function(bool)
+    getgenv().BestPet = bool
+    if bool then
+        BestPet()
+    end
+end)
+
+section3:addButton("Server Hop", function()
+    ServerHop()
+end)
+
+local page = venyx:addPage("Teleports")
+local section4 = page:addSection("Teleports")
+section4:addButton("Teleport Dungeon", function()
+    TeleportDungeon()
+end)
+
+function AutoFarm() 
+    spawn(function()
+        while getgenv().AutoFarm == true do
+            local Event = game:GetService("ReplicatedStorage").Packages.Knit.Services.ClickService.RF.Click
+            Event:InvokeServer()
+        end
+    end)
+end
+
+function EquipBest()
+    spawn(function()
+        while getgenv().AutoFarm == true do
+            local Event = game:GetService("ReplicatedStorage").Packages.Knit.Services.WeaponInvService.RF.EquipBest
+            Event:InvokeServer()
+        end
+    end)
+end
+
+function BestPet()
+    spawn(function()
+        while getgenv().BestPet == true do
+            local Event = game:GetService("ReplicatedStorage").Packages.Knit.Services.PetInvService.RF.EquipBest
+            Event:InvokeServer()
+        end
+    end)
+end
+
+function Egg1()
+    spawn(function()
+        while getgenv().Egg1Hatch == true do
+            local A_1 = 
+            {
+                ["eggName"] = "Egg 1", 
+                ["auto"] = false, 
+                ["amount"] = 1
+            }
+            local Event = game:GetService("ReplicatedStorage").Packages.Knit.Services.EggService.RF.BuyEgg
+            Event:InvokeServer(A_1)
+        end
+    end)
+end
+
+
+function TeleportDungeon()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2837.72314453125, -2.1029701232910156, -130.1952667236328)
+end
+
